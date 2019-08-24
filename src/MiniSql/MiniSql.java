5e819e324faa0d6c342a5e9105340aee2652eb51
Axel Rodriguez <alejandrom9712@gmail.com>
2019-08-24 16:19:51 -0600

    Analizador con error de not match input

--- src/MiniSql/MiniSql.java
+++ src/MiniSql/MiniSql.java
@@ -3,35 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto.compiladores;
-
-import AnalizadorLexico.Lexemas;
-import AnalizadorLexico.MyToken;
-import java.io.BufferedReader;
-import java.io.FileReader;
+package MiniSql;
 
 /**
  *
  * @author axel
  */
-public class ProyectoCompiladores {
+public class MiniSql {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            String archivoAnalizado = "/home/axel/NetBeansProjects/proyecto-compiladores/test.txt";
-            BufferedReader buffered = new BufferedReader(new FileReader(archivoAnalizado));
-            Lexemas analizadorFlex = new Lexemas(buffered);
-            while (true) {
-                MyToken token = analizadorFlex.yylex();
-                if (!analizadorFlex.existenTokens()) break;
-                System.out.println(token.toString());
-            }
-        } catch (Exception e) {
-            System.out.print(e.getMessage());
-        }
+        Principal formPrincipal = new Principal();
+        formPrincipal.setVisible(true);
     }
     
 }
