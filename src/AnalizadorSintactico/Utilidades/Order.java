33baf4c349d2679de4460224ffaa6a0a3f1745a7
Axel Rodriguez <alejandrom9712@gmail.com>
2019-09-28 15:16:29 -0600

    Clase de other_functions funcionando

--- src/AnalizadorSintactico/Utilidades/Order.java
+++ src/AnalizadorSintactico/Utilidades/Order.java
@@ -3,18 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package AnalizadorSintactico.DML;
-
-import AnalizadorSintactico.Utilidades.OtherFunctions;
+package AnalizadorSintactico.Utilidades;
 
 /**
  *
  * @author axel
  */
-public class Select {
+public class Order {
     
-    public void test() {
-        OtherFunctions.OTHFN();
+    public static void ORDER() {
+        
     }
     
 }
---
+++ src/AnalizadorSintactico/Utilidades/OtherFunctions.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorSintactico.Utilidades;
+
+import static AnalizadorSintactico.AnalizadorSintactico.TokenActual;
+import static AnalizadorSintactico.AnalizadorSintactico.moverToken;
+import static AnalizadorSintactico.AnalizadorSintactico.setHasError;
+import MiniSql.Errores;
+
+/**
+ *
+ * @author axel
+ */
+public class OtherFunctions {
+    
+    public static void OTHFN() {
+        switch (TokenActual().get_token()) {
+            case "NEXT":
+                moverToken();
+                if (TokenActual().get_token().equals("VALUE")) {
+                    moverToken();
+                    if (TokenActual().get_token().equals("FOR")) {
+                        moverToken();
+                        if (TokenActual().get_token().equals("Identificador")) {
+                            moverToken();
+                            OTHFN1();
+                        } else {
+                            setHasError(true);
+                            Errores.SyntaxError(TokenActual(), "'FOR'");
+                        }
+                    } else {
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "'VALUE'");
+                    }
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "'NEXT'");
+                }
+                break;
+            case "SYSTEM_USER":
+                moverToken();
+                break;
+            case "SESSION_USER":
+                moverToken();
+                break;
+            case "NULLIF":
+                moverToken();
+                if (TokenActual().get_token().equals("ParentesisAbrir")) {
+                    moverToken();
+                    Expression.EXPRESSION();
+                    if (TokenActual().get_token().equals("Coma")) {
+                        moverToken();
+                        Expression.EXPRESSION();
+                        if (TokenActual().get_token().equals("ParentesisCerrar")) {
+                            moverToken();
+                        } else {
+                            setHasError(true);
+                            Errores.SyntaxError(TokenActual(), "parentesis de cierre");
+                        }
+                    } else {
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "una coma");
+                    }
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "parentesis de apertura");
+                }
+                break;
+            default:
+                
+                break;
+        }
+    }
+    
+    private static void OTHFN1() {
+        switch (TokenActual().get_token()) {
+            case "Punto":
+                moverToken();
+                if (TokenActual().get_token().equals("Identificador")) {
+                    moverToken();
+                    OTHFN2();
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "identificador");
+                }
+                break;
+            default:
+                OTHFN2();
+                break;
+        }
+    }
+    
+    private static void OTHFN2() {
+        switch (TokenActual().get_token()) {
+            case "Punto":
+                moverToken();
+                if (TokenActual().get_token().equals("Identificador")) {
+                    moverToken();
+                    OTHFN3();
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "identificador");
+                }
+                break;
+            default:
+                OTHFN3();
+                break;
+        }
+    }
+    
+    private static void OTHFN3() {
+        if (TokenActual().get_token().equals("OVER")) {
+            moverToken();
+            if (TokenActual().get_token().equals("ORDER")) {
+                moverToken();
+                if (TokenActual().get_token().equals("BY")) {
+                    moverToken();
+                    Order.ORDER();
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "'BY'");
+                }
+            } else {
+                setHasError(true);
+                Errores.SyntaxError(TokenActual(), "'ORDER'");
+            }
+        } else {
+            setHasError(true);
+            Errores.SyntaxError(TokenActual(), "'OVER'");
+        }
+    }
+    
+}
