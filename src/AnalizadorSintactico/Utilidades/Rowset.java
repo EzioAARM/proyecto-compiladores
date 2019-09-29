e434332b28ab2dfd97984bd6b80fdeaeb757d416
Axel Rodriguez <alejandrom9712@gmail.com>
2019-09-29 10:35:59 -0600

    Clase de rowset funcionando

---
+++ src/AnalizadorSintactico/Utilidades/Rowset.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorSintactico.Utilidades;
+
+import static AnalizadorSintactico.AnalizadorSintactico.TokenActual;
+import static AnalizadorSintactico.AnalizadorSintactico.moverToken;
+import static AnalizadorSintactico.AnalizadorSintactico.setHasError;
+import MiniSql.Errores;
+
+/**
+ *
+ * @author axel
+ */
+public class Rowset {
+    
+    public static void ROWSET() {
+        switch (TokenActual().get_token()) {
+            case "OPENDATASOURCE":
+                moverToken();
+                if (TokenActual().get_token().equals("ParentesisAbrir")) {
+                    moverToken();
+                    if (TokenActual().get_token().equals("DatoString")) {
+                        moverToken();
+                        if (TokenActual().get_token().equals("Coma")) {
+                            moverToken();
+                            if (TokenActual().get_token().equals("DatoString")) {
+                                moverToken();
+                                if (TokenActual().get_token().equals("ParentesisCerrar")) {
+                                    moverToken();
+                                } else {
+                                    setHasError(true);
+                                    Errores.SyntaxError(TokenActual(), "parentesis de cierre");
+                                }
+                            } else {
+                                setHasError(true);
+                                Errores.SyntaxError(TokenActual(), "string");
+                            }
+                        } else {
+                            setHasError(true);
+                            Errores.SyntaxError(TokenActual(), "coma");
+                        }
+                    } else {
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "string");
+                    }
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "parentesis de apertura");
+                }
+                break;
+            case "OPENQUERY":
+                moverToken();
+                moverToken();
+                if (TokenActual().get_token().equals("ParentesisAbrir")) {
+                    moverToken();
+                    if (TokenActual().get_token().equals("Identificador")) {
+                        moverToken();
+                        if (TokenActual().get_token().equals("Coma")) {
+                            moverToken();
+                            if (TokenActual().get_token().equals("DatoString")) {
+                                moverToken();
+                                if (TokenActual().get_token().equals("ParentesisCerrar")) {
+                                    moverToken();
+                                } else {
+                                    setHasError(true);
+                                    Errores.SyntaxError(TokenActual(), "parentesis de cierre");
+                                }
+                            } else {
+                                setHasError(true);
+                                Errores.SyntaxError(TokenActual(), "string");
+                            }
+                        } else {
+                            setHasError(true);
+                            Errores.SyntaxError(TokenActual(), "coma");
+                        }
+                    } else {
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "identificador");
+                    }
+                } else {
+                    setHasError(true);
+                    Errores.SyntaxError(TokenActual(), "parentesis de apertura");
+                }
+                break;
+        }
+    }
+    
+}
