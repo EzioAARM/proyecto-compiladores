b0fbb1459622a5694f53f5d2a5ce91bc4ee134be
Axel Rodriguez <alejandrom9712@gmail.com>
2019-09-29 22:03:18 -0600

    DMLSelectList y ColumnName terminado

---
+++ src/AnalizadorSintactico/Utilidades/ColumnName.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorSintactico.Utilidades;
+
+import static AnalizadorSintactico.AnalizadorSintactico.TokenActual;
+import static AnalizadorSintactico.AnalizadorSintactico.moverToken;
+import static AnalizadorSintactico.AnalizadorSintactico.setHasError;
+import MiniSql.Errores;
+
+/**
+ *
+ * @author axel
+ */
+public class ColumnName {
+    
+    public static void COLNAME() {
+        switch (TokenActual().get_token()) {
+            case "Identificador":
+                moverToken();
+                COLNAME1();
+                break;
+        }
+    }
+
+    private static void COLNAME1() {
+        switch (TokenActual().get_token()) {
+            case "Punto":
+                moverToken();
+                switch (TokenActual().get_token()) {
+                    case "Identificador":
+                        moverToken();
+                        break;
+                    case "Multiplicacion":
+                        moverToken();
+                        break;
+                    default:
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "identificador o asterisco");
+                        break;
+                }
+                break;
+        }
+    }
+    
+}
---
+++ src/AnalizadorSintactico/Utilidades/DMLSelectList.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorSintactico.Utilidades;
+
+import static AnalizadorSintactico.AnalizadorSintactico.TokenActual;
+import static AnalizadorSintactico.AnalizadorSintactico.moverToken;
+import static AnalizadorSintactico.AnalizadorSintactico.setHasError;
+import MiniSql.Errores;
+
+/**
+ *
+ * @author axel
+ */
+public class DMLSelectList {
+    
+    public static void DML() {
+        DML2();
+        DML3();
+    }
+    
+    private static void DML2() {
+        switch (TokenActual().get_token()) {
+            case "Identificador":
+                ColumnName.COLNAME();
+                break;
+            default:
+                ScalarExpression.SEXP();
+                break;
+        }
+    }
+    
+    private static void DML3() {
+        switch (TokenActual().get_token()) {
+            case "AS":
+                moverToken();
+                switch (TokenActual().get_token()) {
+                    case "Identificador":
+                        moverToken();
+                        DML4();
+                        break;
+                    default:
+                        setHasError(true);
+                        Errores.SyntaxError(TokenActual(), "identificador");
+                        break;
+                }
+                break;
+        }
+    }
+    
+    private static void DML4() {
+        switch (TokenActual().get_token()) {
+            case "Coma":
+                moverToken();
+                DML();
+                break;
+        }
+    }
+    
+}
