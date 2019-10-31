04ccf4ba6ca85e21dcebd8a56a1aba15ed3fa553
Axel Rodriguez <alejandrom9712@gmail.com>
2019-10-31 02:11:58 -0600

    CREATE procedure funcionando y modificacion a la gramatica de ALTER

---
+++ test/test-cup.sql
@@ -0,0 +1,121 @@
+/*SELECT * FROM miTabla
+GO
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+WHERE OrderDateKey > 20020801  
+ORDER BY OrderDateKey; 
+
+SELECT SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY (OrderDateKey);
+
+INSERT INTO dbo.EmployeeSales  
+    SELECT 'SELECT', sp.BusinessEntityID, c.LastName, sp.SalesYTD   
+    FROM Sales.SalesPerson AS sp  
+    INNER JOIN Person.Person AS c  
+        ON sp.BusinessEntityID = c.BusinessEntityID  
+    WHERE sp.BusinessEntityID LIKE '2%'  
+    ORDER BY sp.BusinessEntityID, c.LastName; 
+
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY OrderDateKey  
+ORDER BY OrderDateKey; 
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY OrderDateKey  
+HAVING OrderDateKey > 20010000  
+ORDER BY OrderDateKey;
+
+
+SELECT @row_count = CONTEO FROM OPENQUERY(ORACLE_LINK, 'SELECT COUNT(employee_id) AS CONTEO FROM JOB_HISTORY');
+
+DELETE Production.ProductCostHistory  
+WHERE StandardCost BETWEEN 12.00 AND 14.00  
+      AND EndDate IS NULL;
+
+UPDATE dbo.Table2   
+SET dbo.Table2.ColB = dbo.Table2.ColB + dbo.Table1.ColB  
+FROM dbo.Table2   
+    INNER JOIN dbo.Table1   
+    ON (dbo.Table2.ColA = dbo.Table1.ColA);  
+
+UPDATE x -- cte is referenced by the alias.  
+SET Value = y.Value  
+FROM cte AS x  -- cte is assigned an alias.  
+INNER JOIN @y AS y ON y.ID = x.ID; 
+
+ALTER TABLE Production.TransactionHistoryArchive
+ADD CONSTRAINT PK_TransactionHistoryArchive_TransactionID PRIMARY KEY CLUSTERED (TransactionID)
+WITH (DATA_COMPRESSION = PAGE);
+
+ALTER TABLE Production.TransactionHistoryArchive
+ADD CONSTRAINT PK_TransactionHistoryArchive_TransactionID PRIMARY KEY CLUSTERED (TransactionID)
+WITH (FILLFACTOR = 75, ONLINE = ON, PAD_INDEX = ON);
+
+ALTER DATABASE AdventureWorks2012
+Modify Name = Northwind ;
+
+ALTER INDEX idxcci_cci_target ON cci_target REORGANIZE WITH (COMPRESS_ALL_ROW_GROUPS = ON);
+
+ALTER INDEX cci_FactInternetSales2 ON FactInternetSales2 REORGANIZE;*/
+
+CREATE PROCEDURE Production.uspGetList @Product VARCHAR(40)   
+    , @MaxPrice VARCHAR   
+    , @ComparePrice VARCHAR OUTPUT  
+    , @ListPrice VARCHAR OUTPUT  
+AS  
+BEGIN
+    SELECT * FROM miTabla
+GO
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+WHERE OrderDateKey > 20020801  
+ORDER BY OrderDateKey; 
+
+SELECT SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY (OrderDateKey);
+
+INSERT INTO dbo.EmployeeSales  
+    SELECT 'SELECT', sp.BusinessEntityID, c.LastName, sp.SalesYTD   
+    FROM Sales.SalesPerson AS sp  
+    INNER JOIN Person.Person AS c  
+        ON sp.BusinessEntityID = c.BusinessEntityID  
+    WHERE sp.BusinessEntityID LIKE '2%'  
+    ORDER BY sp.BusinessEntityID, c.LastName; 
+
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY OrderDateKey  
+ORDER BY OrderDateKey; 
+
+SELECT OrderDateKey, SUM(SalesAmount) AS TotalSales  
+FROM FactInternetSales  
+GROUP BY OrderDateKey  
+HAVING OrderDateKey > 20010000  
+ORDER BY OrderDateKey;
+
+
+SELECT @row_count = CONTEO FROM OPENQUERY(ORACLE_LINK, 'SELECT COUNT(employee_id) AS CONTEO FROM JOB_HISTORY');
+
+DELETE Production.ProductCostHistory  
+WHERE StandardCost BETWEEN 12.00 AND 14.00  
+      AND EndDate IS NULL;
+
+UPDATE dbo.Table2   
+SET dbo.Table2.ColB = dbo.Table2.ColB + dbo.Table1.ColB  
+FROM dbo.Table2   
+    INNER JOIN dbo.Table1   
+    ON (dbo.Table2.ColA = dbo.Table1.ColA);  
+
+UPDATE x -- cte is referenced by the alias.  
+SET Value = y.Value  
+FROM cte AS x  -- cte is assigned an alias.  
+INNER JOIN @y AS y ON y.ID = x.ID; 
+END ;
